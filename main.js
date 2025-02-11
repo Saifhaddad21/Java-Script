84e2f7b7758525758f4e934e3d2c1dc17d478d4d
saif <saif>
2025-02-11 18:28:59 +0300

    output

---
+++ main.js
@@ -0,0 +1,27 @@
+// Output To Screen
+//    Window.alert()
+//    document.write()
+//    console.log()
+
+//    syntax
+
+// window.alert("Hello From JS File");
+document.write("<h1>Hello <span>Page</span></h1>");
+console.log("Hello From JS File");
+console.log("Hello From JS Files");
+
+// Console Methods
+//   log
+//   error
+//   table
+
+//   Web API
+
+//   Styling Console
+//      Directive %c
+
+console.log("log");
+console.error("error");
+console.table(["saif" , "jack" , "jiries"]);
+
+console.log("Hello From %cJS %CFile", "color:red; font-size:40px", "color:blue; font-size:40px");
